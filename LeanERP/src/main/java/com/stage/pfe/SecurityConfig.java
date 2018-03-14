532acf2c8b4e89feeb328811ef2744e4d840032a
ChamiManal <chami.manal.95@gmail.com>
2018-03-14 19:56:17 +0000

    Add files via upload

--- LeanERP/src/main/java/com/stage/pfe/SecurityConfig.java
+++ LeanERP/src/main/java/com/stage/pfe/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll()
 				.and()
 				.logout()
+				.invalidateHttpSession(true)
+				.logoutUrl("/logout")
 				.permitAll();
 
 	}
