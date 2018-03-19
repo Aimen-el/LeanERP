2238f5fe47e5466460f1440b8bd0dbf08b4c5f48
Aimen <mohamed.el-abidi@utbm.fr>
2018-03-19 08:58:45 +0100

    Adding jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,1 +1,21 @@
-//TODO
+pipeline {
+    agent any
+    stages{
+        stage('Build'){
+            steps {
+                sh 'mvn clean package'
+            }
+            post {
+                success {
+                    echo 'Now Archiving...'
+                    archiveArtifacts artifacts: '**/target/*.war'
+                }
+            }
+        }
+        stage ('Deploy to Server'){
+            steps {
+                build job: 'deploy'
+            }
+        }
+    }
+}
