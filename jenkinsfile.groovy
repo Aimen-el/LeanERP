4f664d530a8a0da8d23fe657c3d0e4c53573c72a
Aimen <mohamed.el-abidi@utbm.fr>
2018-03-19 11:32:40 +0100

    deployement by step

---
+++ jenkinsfile.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'tomcat', defaultValue: '35.180.47.231', description: 'Staging Server')
+    }
+
+    triggers {
+        pollSCM('* * * * *')
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+            post {
+                success {
+                    echo 'Now Archiving...'
+                    archiveArtifacts artifacts: '**/target/*.war'
+                }
+            }
+        }
+        stage('Deploy to Staging') {
+            steps {
+                sh "scp -i /Users/aimen/Downloads/tomcat.pem **/target/*.war ec2-user@${params.tomcat}:/var/lib/tomcat7/webapps"
+            }
+        }
+    }
+}
