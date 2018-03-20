bdd41ccb3168cee40ed9cdbbf1e0979dc16889cc
Aimen <mohamed.el-abidi@utbm.fr>
2018-03-20 15:27:17 +0100

    Fix google auth

--- src/main/java/com/stage/pfe/SecurityConfig.java
+++ src/main/java/com/stage/pfe/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.stage.pfe;
 
+import javax.servlet.Filter;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -12,16 +19,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-/**
- * Modifying or overriding the default spring boot security.
- */
-@Configurable
+
+@Configuration
 @EnableWebSecurity
+@EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     private OAuth2ClientContext oauth2ClientContext;
     private AuthorizationCodeResourceDetails authorizationCodeResourceDetails;
@@ -63,7 +72,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-
         http
                 // Starts authorizing configurations.
                 .authorizeRequests()
@@ -73,7 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin()
-                .loginPage("/google/login")
+                .loginPage("/login")
                 .permitAll()
                 .and()
                 // Setting the logout URL "/logout" - default logout URL.
@@ -87,6 +95,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterAt(filter(), BasicAuthenticationFilter.class)
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+
 
     }
 
@@ -109,4 +119,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         return oAuth2Filter;
     }
+
+	/*@Autowired
+	DataSource datasource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(datasource)
+				.usersByUsernameQuery("select username ,password , enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?")
+				.rolePrefix("");
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/css/**", "/js/**", "/image/**").permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+				.logout()
+				.invalidateHttpSession(true)
+				.logoutUrl("/logout")
+				.permitAll();
+
+	}*/
 }
