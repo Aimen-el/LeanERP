bf933cfffd69451b46c3e1e003625d49ae7accd9
Aimen <mohamed.el-abidi@utbm.fr>
2018-03-23 13:55:03 +0100

    erpdb

--- erpdb.sql
+++ erpdb.sql
@@ -15,8 +15,7 @@ create table user_roles
 (
 	role_id int auto_increment
 		primary key,
-	role varchar(45) default 'CONSULTANT' not null,
-	users varchar(45) null
+	role varchar(45) default 'CONSULTANT' not null
 )
 ;
 
@@ -29,7 +28,7 @@ create table users
 	password varchar(45) null,
 	photo varchar(255) null,
 	principal_id varchar(255) null,
-	role_id int default '2' not null,
+	role_id int null,
 	roles varchar(45) default 'CONSULTANT' not null,
 	constraint FKh555fyoyldpyaltlb7jva35j2
 		foreign key (role_id) references user_roles (role_id),
@@ -40,24 +39,5 @@ create table users
 
 create index role_id_idx
 	on users (role_id)
-;
-
-create table users_roles
-(
-	user_id int not null,
-	role_id int not null,
-	constraint FK2o0jvgh89lemvvo17cbqvdxaa
-		foreign key (user_id) references users (user_id),
-	constraint FKfivrl5i32jkvd1w39y4h2vn90
-		foreign key (role_id) references user_roles (role_id)
-)
-;
-
-create index FK2o0jvgh89lemvvo17cbqvdxaa
-	on users_roles (user_id)
-;
-
-create index FKfivrl5i32jkvd1w39y4h2vn90
-	on users_roles (role_id)
 ;
 
