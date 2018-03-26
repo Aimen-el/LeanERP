1d8140110e96b06af080ea26e461127b7bed8f85
ChamiManal <chami.manal.95@gmail.com>
2018-03-26 11:56:53 +0200

    retrieve consultant

--- src/main/java/com/stage/pfe/dao/UserRepository.java
+++ src/main/java/com/stage/pfe/dao/UserRepository.java
@@ -1,14 +1,13 @@
 package com.stage.pfe.dao;
 
-import com.stage.pfe.entities.NoteFrais;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.stage.pfe.entities.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
     User findByPrincipalId(String principalId);
+    List<User> findAllByName(String name);
 
 }
