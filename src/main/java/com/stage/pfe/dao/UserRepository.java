9691a12af0d87f139158ba0b19f8d09c0f51b52a
ChamiManal <chami.manal.95@gmail.com>
2018-03-27 13:34:28 +0200

    Update

--- src/main/java/com/stage/pfe/dao/UserRepository.java
+++ src/main/java/com/stage/pfe/dao/UserRepository.java
@@ -1,6 +1,10 @@
 package com.stage.pfe.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.stage.pfe.entities.User;
 
@@ -9,4 +13,16 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, String> {
     User findByPrincipalId(String principalId);
     List<User> findAllByName(String name);
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE User user SET user.soldeConges = :soldeConges, user.jourDeRecup = :jourDeRecup,"
+    		+ " user.mission = :mission, user.CertificationObtenues = :CertificationObtenues, "
+    		+ "user.CertificationEnCours = :CertificationEnCours, user.livre = :livre, user.noteFrais = :noteFrais"
+    		+ " WHERE user.principalId = :id")
+    int updateUser(@Param("soldeConges") long soldeConges, @Param("jourDeRecup") long jourDeRecup,
+    		@Param("mission") String mission, 
+    		@Param("CertificationObtenues") String CertificationObtenues, 
+    		@Param("CertificationEnCours") String CertificationEnCours,
+    		@Param("livre") String livre,
+    		@Param("noteFrais") String noteFrais);
 }
