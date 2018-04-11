440bb315858f73524f294e8c3be024f304ca17ce
SOSPC <chami.manal.95@gmail.com>
2018-04-11 17:22:39 +0100

    First Commit

---
+++ src/main/java/com/stage/pfe/entities/Role.java
@@ -0,0 +1,42 @@
+package com.stage.pfe.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user_roles")
+public class Role implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int role_id;
+	private String role;
+
+
+	public Role(String role) {
+		this.role = role;
+	}
+
+	public Role() {
+	}
+
+	public int getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
